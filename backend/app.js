df0a5bad63e99d09904e4729b8c0e6e2450737d6
Ekaterina Anikeeva <ekaanikeeva@yandex.ru>
2021-11-07 23:19:55 +0300

    fead crash test

--- backend/app.js
+++ backend/app.js
@@ -32,6 +32,11 @@ app.use(express.json());
 
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 app.post('/signin', validateSignIn, login);
 app.post('/signup', validateSignIn, createUser);
 app.use(auth);
